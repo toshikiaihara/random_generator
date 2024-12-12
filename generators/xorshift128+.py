4793bb336b29fa582793321625e44dcf4f50f003
toshikiaihara <2toshiki0718@gmail.com>
2024-12-13 01:32:40 +0900

    fix xorshift128+ output

--- generators/xorshift128+.py
+++ generators/xorshift128+.py
@@ -1,10 +1,11 @@
 class XorShift128Plus:
-    _BIT_MASK = 2**128 - 1
+    _STATE_BIT_MASK = 2**128 - 1
+    _OUTPUT_MASK = 2**64 - 1
 
     def __init__(self, seed1: int, seed2: int, a: int = 23, b: int = 18, c: int = 5):
         self.state = [
-            seed1 & self._BIT_MASK,
-            seed2 & self._BIT_MASK,
+            seed1 & self._STATE_BIT_MASK,
+            seed2 & self._STATE_BIT_MASK,
         ]
         self.a = a
         self.b = b
@@ -15,19 +16,22 @@ class XorShift128Plus:
             raise ValueError("Shift amount must be greater than 0")
 
         if dimension == "left":
-            return value ^ (value << shift_amount) & self._BIT_MASK
+            return value ^ (value << shift_amount) & self._STATE_BIT_MASK
         elif dimension == "right":
             return value ^ (value >> shift_amount)
         raise ValueError(f"Invalid dimension: {dimension}")
 
-    def generate(self):
+    def generate(self, output_format: str | None = None):
         old_s0, old_s1 = self.state
-        output = (old_s0 + old_s1) & self._BIT_MASK
+        output = (old_s0 + old_s1) & self._OUTPUT_MASK
 
         self.state[0] = old_s1
         old_s0_ILa = self.__xorshift(old_s0, "left", self.a)
         old_s0_ILa_IRb = self.__xorshift(old_s0_ILa, "right", self.b)
         old_s1_IRc = self.__xorshift(old_s1, "right", self.c)
         self.state[1] = old_s0_ILa_IRb ^ old_s1_IRc
+
+        if output_format == "unit":
+            return output / self._OUTPUT_MASK
 
         return output
